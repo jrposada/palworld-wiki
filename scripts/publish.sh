173f6fa2a855a24967705b89333f7960efe7ee39
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-02-04 23:28:36 +0100

    feat: create dummy frontend dockerfile and update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,9 +7,20 @@ if [ "$version" == "null" ]; then
     exit 1
 fi
 
+echo "Backend..."
 docker buildx build \
+    --file ../Dockerfile.backend \
     --platform linux/amd64,linux/arm64 \
-    -t jrposada/palworld-wiki:$version \
-    -t jrposada/palworld-wiki:latest \
+    -t jrposada/palworld-wiki-backend:$version \
+    -t jrposada/palworld-wiki-backend:latest \
+    --push \
+    .
+
+echo "Frontend..."
+docker buildx build \
+    --file ../Dockerfile.frontend \
+    --platform linux/amd64,linux/arm64 \
+    -t jrposada/palworld-wiki-frontend:$version \
+    -t jrposada/palworld-wiki-frontend:latest \
     --push \
     .
