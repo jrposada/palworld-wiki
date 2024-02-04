7041fff8e7efcdb8acea7c0030ee7a9c387c3388
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-02-04 01:32:34 +0100

    build: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,10 @@
 
 # This is meant to be run in the server.
 
+echo "npm ci"
+npm ci
+
 npm run build --workspace=frontend
+
+rm -rf /var/www/palworld-wiki/html/dist
+cp -r ./frontend/dist /var/www/palworld-wiki/html
