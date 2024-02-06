06c0120cdb12cac0e547815c97132e7886158167
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2024-02-06 19:37:45 +0100

    feat: configure sequelize-cli

---
+++ backend/config/config.cjs
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        username: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DATABASE,
+        host: process.env.PG_HOST,
+        dialect: 'postgres',
+        port: process.env.PG_HOST_PORT,
+    },
+};
